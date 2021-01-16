2e1472d3c6fa245ea2a25bb76f451dd9f9850cdf
Volodymyr <volodymyrquo@gmail.com>
2021-01-16 07:43:30 +0200

    learning evernts

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-const sum = require('./tutorial');
-const tutorial = require('./tutorial');
-console.log(tutorial);
+const EventEmmiter = require('events');
+const eventEmmiter = new EventEmmiter();
+
+eventEmmiter.on('tutorial', (num1) => {
+  console.log(num1);
+});
+
+eventEmmiter.emit('tutorial', 'Hello everyone!');
+
+class Person extends EventEmmiter {
+  constructor(name) {
+    super();
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+}
+
+let pedro = new Person('Pedro');
+let christina = new Person('Christina');
+
+pedro.on('name', () => {
+  console.log('my name is ' + pedro.name);
+});
+christina.on('name', () => {
+  console.log('my name is ' + christina.name);
+});
+
+pedro.emit('name');
+christina.emit('name');
