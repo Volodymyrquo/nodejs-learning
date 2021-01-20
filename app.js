42b1153fda3b76a2dd1a509fb147d39ba2e3e30d
Volodymyr <volodymyrquo@gmail.com>
2021-01-20 12:54:28 +0200

    gzip and gunzip

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('./example.txt', 'utf8');
-const writeStream = fs.createWriteStream('example_two.txt');
-
-readStream.on('data', (chunk) => {
+const zlib = require('zlib');
+const gunzip = zlib.createGunzip();
+/* const readStream = fs.createReadStream('./example.txt', 'utf8');
+const writeStream = fs.createWriteStream('example_three.txt.gz');
+ */
+/* readStream.on('data', (chunk) => {
   writeStream.write(chunk);
-});
+}); */
+
+/* readStream.pipe(writeStream); */
+
+/* readStream.pipe(gzip).pipe(writeStream);
+ */
+
+const readStream = fs.createReadStream('./example_three.txt.gz');
+const writeStream = fs.createWriteStream('uncompressed.txt');
+
+readStream.pipe(gunzip).pipe(writeStream);
