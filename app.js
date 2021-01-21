0347d9c0c107fa426ac846571a9a34ea0b400e81
Volodymyr <volodymyrquo@gmail.com>
2021-01-21 04:44:32 +0200

    express getting post data

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/public', express.static(__dirname + '/static_1'));
+app.use('/public', express.static(path.join(__dirname, 'static_form')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static_1' + '/index.html');
+  res.sendFile(path.join(__dirname, 'static_form', 'index.html'));
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  res.send('successfully posted data');
 });
 
 app.listen(8000);
